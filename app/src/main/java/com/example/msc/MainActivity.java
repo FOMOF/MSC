41f76c730b87a95214f90930e283d811b6106937
FOMOF <wrag@bk.ru>
2024-05-04 19:06:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/msc/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.msc;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://fomof.ru/app/index.html");
+
+        WebViewClient webViewClient = new WebViewClient() {
+
+            @SuppressWarnings("deprecation") @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                view.loadUrl(url);
+
+                return true;
+
+            }
+
+
+            @TargetApi(Build.VERSION_CODES.N) @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                view.loadUrl(request.getUrl().toString());
+
+                return true;
+
+            }
+
+        };
+        webView.setWebViewClient(webViewClient);
+        getSupportActionBar().hide();
+    }
+}
